0072d55047c659637e60991d39d129dea617d2c3
VaderNgo <133780416+VaderNgo@users.noreply.github.com>
2024-03-20 23:40:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ filename = 'example.txt'
 
 with open(filename, 'a') as f:
     now = datetime.now()
-    f.write(f'Today is {now.strftime("%Y-%m-%d")}\n')
+    f.write(f'Updating: {now.strftime("%Y-%m-%d")}\n')
